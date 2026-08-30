3dc3a9b0b9e06c82c8d9737f8c90a214a86092dc
mountuint <mountuint@protonmail.com>
2026-08-30 17:25:51 +0300

    Complete the addMarkToBoard function

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const Gameboard = {
-    board: [],
-    
-    
+  board: Array(9),
+};
+
+function addMarkToBoard(player, position) {
+  if (player === "O") {
+    Gameboard.board[position] = "O";
+  } else if (player === "X") {
+    Gameboard.board[position] = "X";
+  }
 }
