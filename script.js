b5d690f82f2c5afc50807400f6b0984ddf02a620
mountuint <mountuint@protonmail.com>
2026-09-04 13:04:31 +0300

    Add player score variables

--- script.js
+++ script.js
@@ -11,11 +11,10 @@ function addMarkToBoard(player, position) {
   }
 }
 
-addMarkToBoard("X", 0);
-addMarkToBoard("O", 1);
-addMarkToBoard("X", 2);
-addMarkToBoard("X", 4);
-addMarkToBoard("X", 8);
+
+let firstPlayerScore = 0;
+let secondPlayerScore = 0
+
 
 function checkTheBoard() {
   let lenghtOfRow = Gameboard.board.length / 3;
@@ -27,45 +26,78 @@ function checkTheBoard() {
     (firstRow[0] === "X" && firstRow[1] === "X" && firstRow[2] === "X") ||
     (firstRow[0] === "O" && firstRow[1] === "O" && firstRow[2] === "O")
   ) {
-    console.log("You have won");
+    firstRow[0] === "X" ? firstPlayerScore += 1 : secondPlayerScore += 1;
+    console.log(`This is the first player score: ${firstPlayerScore}`)
+    console.log(`This is the second player score: ${secondPlayerScore}`)
   } else if (
     (secondRow[0] === "X" && secondRow[1] === "X" && secondRow[2] === "X") ||
     (secondRow[0] === "O" && secondRow[1] === "O" && secondRow[2] === "O")
   ) {
-    console.log("You have won again!!");
+    secondRow[0] === "X" ? firstPlayerScore += 1 : secondPlayerScore += 1;
+    console.log(`This is the first player score: ${firstPlayerScore}`)
+    console.log(`This is the second player score: ${secondPlayerScore}`)
   } else if (
     (thirdRow[0] === "X" && thirdRow[1] === "X" && thirdRow[2] === "X") ||
     (thirdRow[0] === "O" && thirdRow[1] === "O" && thirdRow[2] === "O")
   ) {
-    console.log("You are amazing");
+    thirdRow[0] === "X" ? firstPlayerScore += 1 : secondPlayerScore += 1;
+    console.log(`This is the first player score: ${firstPlayerScore}`)
+    console.log(`This is the second player score: ${secondPlayerScore}`)
   } else if (
     (firstRow[0] === "X" && secondRow[1] === "X" && thirdRow[2] === "X") ||
     (firstRow[0] === "O" && secondRow[1] === "O" && thirdRow[2] === "O")
   ) {
-    console.log("You have won with first cross");
+    firstRow[0] === "X" ? firstPlayerScore += 1 : secondPlayerScore += 1;
+    console.log(`This is the first player score: ${firstPlayerScore}`)
+    console.log(`This is the second player score: ${secondPlayerScore}`)
   } else if (
     (firstRow[2] === "X" && secondRow[1] === "X" && thirdRow[0] === "X") ||
     (firstRow[2] === "O" && secondRow[1] === "O" && thirdRow[0] === "O")
   ) {
-    console.log("You have won with second cross");
+    firstRow[2] === "X" ? firstPlayerScore += 1 : secondPlayerScore += 1;
+    console.log(`This is the first player score: ${firstPlayerScore}`)
+    console.log(`This is the second player score: ${secondPlayerScore}`)
   } else if (
     (firstRow[1] === "X" && secondRow[1] === "X" && thirdRow[1] === "X") ||
     (firstRow[1] === "O" && secondRow[1] === "O" && thirdRow[1] === "O")
   ) {
-    console.log("You have won with vertical middle");
+    firstRow[1] === "X" ? firstPlayerScore += 1 : secondPlayerScore += 1;
+    console.log(`This is the first player score: ${firstPlayerScore}`)
+    console.log(`This is the second player score: ${secondPlayerScore}`)
   } else if (
     (firstRow[0] === "X" && secondRow[0] === "X" && thirdRow[0] === "X") ||
     (firstRow[0] === "O" && secondRow[0] === "O" && thirdRow[0] === "O")
   ) {
-    console.log("You have won with vertical first");
+    firstRow[0] === "X" ? firstPlayerScore += 1 : secondPlayerScore += 1;
+    console.log(`This is the first player score: ${firstPlayerScore}`)
+    console.log(`This is the second player score: ${secondPlayerScore}`)
   } else if (
     (firstRow[2] === "X" && secondRow[2] === "X" && thirdRow[2] === "X") ||
     (firstRow[2] === "O" && secondRow[2] === "O" && thirdRow[2] === "O")
   ) {
-    console.log("You have won with vertical last");
+    firstRow[2] === "X" ? firstPlayerScore += 1 : secondPlayerScore += 1
+    console.log(`This is the first player score: ${firstPlayerScore}`)
+    console.log(`This is the second player score: ${secondPlayerScore}`)
   }
 }
 
-checkTheBoard();
+function playTheGame() {
+  let firstPlayer = "X";
+  let secondPlayer = "O";
+
+  addMarkToBoard("X", 0);
+  addMarkToBoard("O", 1);
+  addMarkToBoard("X", 2);
+  addMarkToBoard("X", 3);
+  addMarkToBoard("O", 4);
+  addMarkToBoard("X", 5);
+  addMarkToBoard("O", 6);
+  addMarkToBoard("X", 7);
+  addMarkToBoard("X", 8);
+
+  checkTheBoard();
+}
+
+playTheGame();
 
 console.log(Gameboard.board);
