9e597634ffa8348b31bac764fcfc1e22a2466956
mountuint <mountuint@protonmail.com>
2026-09-11 18:01:41 +0300

    Add start game button functionality and remove unnecessary lines

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const squareContainer = document.querySelector("#square-container");
 const squares = document.querySelectorAll(".square");
 const playerContainer = document.querySelector("#player-container");
-const players = document.querySelectorAll(".player");
+const firstPlayer = document.querySelector("#first-player");
+const secondPlayer = document.querySelector("#second-player");
+const turnIndicator = document.querySelector("#turn-indicator");
+const startButton = document.querySelector("#start-button");
 
 let player = "X";
 let firstPlayerScore = 0;
@@ -10,6 +13,14 @@ let result = false;
 
 const Gameboard = {
   board: Array(9),
+
+  startTheGame() {
+    startButton.addEventListener("click", () => {
+      if (firstPlayer.value !== "" && secondPlayer.value !== "") {
+        turnIndicator.textContent = `${firstPlayer.value}'s turn!`;
+      }
+    });
+  },
 
   addMarkToBoard(position) {
     if (result === true) {
@@ -21,9 +32,11 @@ const Gameboard = {
     if (Gameboard.board[position] !== undefined) return;
     if (player === "O") {
       Gameboard.board[position] = "O";
+      turnIndicator.textContent = `${firstPlayer.value}'s turn!`;
       player = "X";
     } else if (player === "X") {
       Gameboard.board[position] = "X";
+      turnIndicator.textContent = `${secondPlayer.value}'s turn!`;
       player = "O";
     }
   },
@@ -39,76 +52,107 @@ const Gameboard = {
       (firstRow[0] === "O" && firstRow[1] === "O" && firstRow[2] === "O")
     ) {
       firstRow[0] === "X" ? (firstPlayerScore += 1) : (secondPlayerScore += 1);
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      if (firstPlayerScore === 1) {
+        turnIndicator.textContent = `${firstPlayer.value} wins!`;
+      } else {
+        turnIndicator.textContent = `${secondPlayer.value} wins!`;
+        secondPlayerScore = 0;
+      }
       result = true;
     } else if (
       (secondRow[0] === "X" && secondRow[1] === "X" && secondRow[2] === "X") ||
       (secondRow[0] === "O" && secondRow[1] === "O" && secondRow[2] === "O")
     ) {
       secondRow[0] === "X" ? (firstPlayerScore += 1) : (secondPlayerScore += 1);
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      if (firstPlayerScore === 1) {
+        turnIndicator.textContent = `${firstPlayer.value} wins!`;
+      } else {
+        turnIndicator.textContent = `${secondPlayer.value} wins!`;
+        secondPlayerScore = 0;
+      }
       result = true;
     } else if (
       (thirdRow[0] === "X" && thirdRow[1] === "X" && thirdRow[2] === "X") ||
       (thirdRow[0] === "O" && thirdRow[1] === "O" && thirdRow[2] === "O")
     ) {
       thirdRow[0] === "X" ? (firstPlayerScore += 1) : (secondPlayerScore += 1);
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      if (firstPlayerScore === 1) {
+        turnIndicator.textContent = `${firstPlayer.value} wins!`
+      } else {
+        turnIndicator.textContent = `${secondPlayer.value} wins!`
+        secondPlayerScore = 0
+      }
       result = true;
     } else if (
       (firstRow[0] === "X" && secondRow[1] === "X" && thirdRow[2] === "X") ||
       (firstRow[0] === "O" && secondRow[1] === "O" && thirdRow[2] === "O")
     ) {
       firstRow[0] === "X" ? (firstPlayerScore += 1) : (secondPlayerScore += 1);
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      if (firstPlayerScore === 1) {
+        turnIndicator.textContent = `${firstPlayer.value} wins!`
+      } else {
+        turnIndicator.textContent = `${secondPlayer.value} wins!`
+        secondPlayerScore = 0
+      }
       result = true;
     } else if (
       (firstRow[2] === "X" && secondRow[1] === "X" && thirdRow[0] === "X") ||
       (firstRow[2] === "O" && secondRow[1] === "O" && thirdRow[0] === "O")
     ) {
       firstRow[2] === "X" ? (firstPlayerScore += 1) : (secondPlayerScore += 1);
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      if (firstPlayerScore === 1) {
+        turnIndicator.textContent = `${firstPlayer.value} wins!`
+      } else {
+        turnIndicator.textContent = `${secondPlayer.value} wins!`
+        secondPlayerScore = 0
+      }
       result = true;
     } else if (
       (firstRow[1] === "X" && secondRow[1] === "X" && thirdRow[1] === "X") ||
       (firstRow[1] === "O" && secondRow[1] === "O" && thirdRow[1] === "O")
     ) {
       firstRow[1] === "X" ? (firstPlayerScore += 1) : (secondPlayerScore += 1);
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      if (firstPlayerScore === 1) {
+        turnIndicator.textContent = `${firstPlayer.value} wins!`
+      } else {
+        turnIndicator.textContent = `${secondPlayer.value} wins!`
+        secondPlayerScore = 0
+      }
       result = true;
     } else if (
       (firstRow[0] === "X" && secondRow[0] === "X" && thirdRow[0] === "X") ||
       (firstRow[0] === "O" && secondRow[0] === "O" && thirdRow[0] === "O")
     ) {
       firstRow[0] === "X" ? (firstPlayerScore += 1) : (secondPlayerScore += 1);
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      if (firstPlayerScore === 1) {
+        turnIndicator.textContent = `${firstPlayer.value} wins!`
+      } else {
+        turnIndicator.textContent = `${secondPlayer.value} wins!`
+        secondPlayerScore = 0
+      }
       result = true;
     } else if (
       (firstRow[2] === "X" && secondRow[2] === "X" && thirdRow[2] === "X") ||
       (firstRow[2] === "O" && secondRow[2] === "O" && thirdRow[2] === "O")
     ) {
       firstRow[2] === "X" ? (firstPlayerScore += 1) : (secondPlayerScore += 1);
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      if (firstPlayerScore === 1) {
+        turnIndicator.textContent = `${firstPlayer.value} wins!`
+      } else {
+        turnIndicator.textContent = `${secondPlayer.value} wins!`
+        secondPlayerScore = 0
+      }
       result = true;
     } else if (
       Gameboard.board.filter((element) => element !== undefined).length === 9
     ) {
-      console.log("It is a tie!!!");
-      console.log(`This is the first player score: ${firstPlayerScore}`);
-      console.log(`This is the second player score: ${secondPlayerScore}`);
+      turnIndicator.textContent = "It is a tie!!!"
       result = true;
     }
   },
 
   playTheGame() {
+    this.startTheGame();
     displayController.clickTheBoard();
   },
 };
